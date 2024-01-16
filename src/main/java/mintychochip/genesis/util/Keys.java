ba53aa0ae54f50926b8ef5704f2278caa2cc8222
Justin Lo <jlo2@csub.edu>
2024-01-16 21:41:22 +0000

    added more functionality for keys

--- src/main/java/mintychochip/genesis/util/Keys.java
+++ src/main/java/mintychochip/genesis/util/Keys.java
@@ -2,6 +2,9 @@ package mintychochip.genesis.util;
 
 import mintychochip.genesis.Genesis;
 import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
@@ -9,17 +12,46 @@ import java.util.Map;
 
 public class Keys {
     private Map<String, NamespacedKey> keys = new HashMap();
+
     public Keys() {
-        generateKey(Genesis.getInstance(),"unstackable");
+        generateKey(Genesis.getInstance(), "unstackable");
     }
 
     public NamespacedKey generateKey(JavaPlugin plugin, String key) {
-        return new NamespacedKey(plugin,key);
+        return new NamespacedKey(plugin, key);
     }
+
     public void putKey(NamespacedKey namespacedKey, String key) {
-        keys.put(key,namespacedKey);
+        keys.put(key, namespacedKey);
     }
+
     public Map<String, NamespacedKey> getMap() {
         return keys;
+    }
+
+    public boolean assignStringToItemStack(String genesisKey, String value, ItemStack itemStack) {
+        return assignGenericToItemStack(genesisKey,PersistentDataType.STRING,value,itemStack);
+    }
+    public boolean assignIntegerToItemStack(String genesisKey, Integer value, ItemStack itemStack) {
+        return assignGenericToItemStack(genesisKey,PersistentDataType.INTEGER,value,itemStack);
+    }
+    public boolean assignByteArrayToItemStack(String genesisKey, byte[] value, ItemStack itemStack) {
+        return assignGenericToItemStack(genesisKey,PersistentDataType.BYTE_ARRAY,value,itemStack);
+    }
+    public boolean assignBooleanToItemStack(String genesisKey, boolean value, ItemStack itemStack) {
+        return assignGenericToItemStack(genesisKey,PersistentDataType.BOOLEAN,value,itemStack);
+    }
+    public <T> boolean assignGenericToItemStack(String genesisKey, PersistentDataType persistentDataType, T value, ItemStack itemStack) {
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        if (itemMeta == null) {
+            return false;
+        }
+        if(keys.get(genesisKey) == null) {
+            return false;
+            //can do error message here
+        }
+        itemMeta.getPersistentDataContainer().set(keys.get(genesisKey),persistentDataType,value);
+        itemStack.setItemMeta(itemMeta);
+        return true;
     }
 }
---
+++ src/main/java/mintychochip/genesis/util/StripLog.java
@@ -0,0 +1,57 @@
+package mintychochip.genesis.util;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.Damageable;
+
+public class StripLog implements Listener {
+
+    @EventHandler
+    public void onPlayerInteractEvent(final PlayerInteractEvent event) {
+        if (event.getAction() != Action.RIGHT_CLICK_AIR || event.getAction() != Action.RIGHT_CLICK_BLOCK) {
+            return;
+        }
+        Player player = event.getPlayer();
+        PlayerInventory inventory = player.getInventory();
+        ItemStack itemInMainHand = inventory.getItemInMainHand();
+        ItemStack itemInOffHand = inventory.getItemInOffHand();
+        if (itemInMainHand.getType() == Material.AIR || itemInOffHand.getType() == Material.AIR) {
+            return;
+        }
+        if(canStrip(itemInMainHand,itemInOffHand) || canStrip(itemInOffHand,itemInMainHand)) { //checks the first case where the player holds axe in main, and the log is in offhand
+            ItemStack logs = itemStackWithString(itemInMainHand, itemInOffHand,"LOG");
+            logs.setType(Material.valueOf("STRIPPED_" + logs.getType()));
+            ItemStack axe = itemStackWithString(itemInMainHand,itemInOffHand,"AXE");
+            if(axe.getItemMeta() instanceof Damageable damageable) {
+                int enchantLevel = damageable.getEnchantLevel(Enchantment.DURABILITY);
+                double damage = logs.getAmount();
+                if(enchantLevel > 0) {
+                    damage /= enchantLevel;
+                }
+                damageable.setDamage(damageable.getDamage() - Math.round((float) damage));
+            }
+        }
+    }
+
+    public boolean canStrip(ItemStack mainHand, ItemStack offHand) {
+        String main = mainHand.getType().toString();
+        String off = offHand.getType().toString();
+        if(off.contains("STRIPPED")) {
+            return false;
+        }
+        if(off.contains("LOG") && main.contains("AXE")) {
+            return true;
+        }
+        return false;
+    }
+    public ItemStack itemStackWithString(ItemStack mainHand, ItemStack offHand, String match) {
+        return mainHand.getType().toString().contains(match.toUpperCase()) ? mainHand : offHand;
+    }
+}
